1de2f53f6dd7a7832ca25b07cf59b9eb32104aa3
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-02-28 19:03:39 +0530

    created sequential model

--- main.py
+++ main.py
@@ -34,3 +34,32 @@ dataset = dataset.prefetch(8) # helps bottlenecks
 train = dataset.take(int(len(dataset)*.7))
 val = dataset.skip(int(len(dataset)*.7)).take(int(len(dataset)*.2))
 test = dataset.skip(int(len(dataset)*.9)).take(int(len(dataset)*.1))
+
+# Create Sequential Model
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dropout, Bidirectional, Dense, Embedding
+
+
+model = Sequential()
+# Create the embedding layer 
+model.add(Embedding(MAX_FEATURES+1, 32))
+# Bidirectional LSTM Layer
+model.add(Bidirectional(LSTM(32, activation='tanh')))
+# Feature extractor Fully connected layers
+model.add(Dense(128, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
+# Final layer 
+model.add(Dense(6, activation='sigmoid'))
+
+model.compile(loss='BinaryCrossentropy', optimizer='Adam')
+
+model.summary()
+
+history = model.fit(train, epochs=1, validation_data=val)
+
+from matplotlib import pyplot as plt
+plt.figure(figsize=(8,5))
+pd.DataFrame(history.history).plot()
+plt.show()
